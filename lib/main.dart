bb05d8cfb39cab1fcfeaec24e91a107f83098a0d
SANGWOO PARK <cyberprophet@hanmail.net>
2023-03-01 04:23:45 +0900

    contemplate a theme

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,78 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void main() {
   runApp(const App());
 }
 
-class App extends StatelessWidget {
+class App extends StatefulWidget {
   const App({super.key});
 
   @override
+  State<App> createState() => _AppState();
+}
+
+class _AppState extends State<App> {
+  @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      themeMode: themeMode,
       title: 'Algorithmic-Trading',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        useMaterial3: true,
+        appBarTheme: const AppBarTheme(
+          backgroundColor: Colors.white,
+          systemOverlayStyle: SystemUiOverlayStyle(
+            statusBarBrightness: Brightness.light,
+            statusBarColor: Colors.transparent,
+            statusBarIconBrightness: Brightness.dark,
+          ),
+          titleTextStyle: TextStyle(
+            fontSize: 16 + 8,
+            fontWeight: FontWeight.bold,
+            color: Colors.black,
+          ),
+        ),
+        primaryColor: Colors.black,
       ),
-      home: const Scaffold(),
+      darkTheme: ThemeData(
+        useMaterial3: true,
+        appBarTheme: const AppBarTheme(
+          backgroundColor: Colors.black,
+          titleTextStyle: TextStyle(
+            color: Colors.white,
+            fontSize: 16 + 8,
+            fontWeight: FontWeight.bold,
+          ),
+          systemOverlayStyle: SystemUiOverlayStyle(
+            statusBarBrightness: Brightness.dark,
+            statusBarColor: Colors.transparent,
+            statusBarIconBrightness: Brightness.light,
+          ),
+        ),
+        primaryColor: Colors.white,
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          centerTitle: true,
+          title: const Text(
+            'login',
+          ),
+        ),
+      ),
+      debugShowCheckedModeBanner: false,
     );
   }
+
+  @override
+  initState() {
+    super.initState();
+    themeMode = ThemeMode.system;
+  }
+
+  void changeTheme() {
+    setState(() => themeMode = ThemeMode.system);
+  }
+
+  late ThemeMode themeMode;
 }
